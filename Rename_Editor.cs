2b057191d0e057ab70678dae81e36217784c83e2
Mükremin Remezan YILANCI <41707639+mryilanc1@users.noreply.github.com>
2022-02-13 22:41:52 +0300

    Rename Selection objects

---
+++ Rename_Editor.cs
@@ -0,0 +1,96 @@
+
+
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+
+
+
+public class Rename_Editor : EditorWindow
+    {
+
+        static string location = "";
+        static string localPath;
+        private static string guide = "Select GameObjects";
+        public static GameObject[] objectArray;
+    private static Vector2 _windowsMinSize = Vector2.one * 500f;
+    private static Rect _helpRect = new Rect(0f, 0f, 400f, 100f);
+    private static Rect _listRect = new Rect(Vector2.zero, _windowsMinSize);
+
+    // Creates a new menu item 'Multiple Prefab/Multiple Prefab Creator  Window' in the main menu and create a shortcut.
+    [MenuItem("Rename/Rename_Editor Window")]
+        static void Init()
+        {
+
+        // Get existing open window or if none, make a new one:
+        Rename_Editor window = (Rename_Editor) EditorWindow.GetWindow(typeof(Rename_Editor),true, "Rename_Editor");
+          
+            window.Show();
+            
+        }
+        
+        void OnGUI()
+        {
+
+      
+
+            location = EditorGUILayout.TextField("Name:", location);
+            if (Selection.activeGameObject != null)
+            {
+                GUI.backgroundColor= Color.green;
+                
+            }
+            else
+            {
+                GUI.backgroundColor= Color.red;
+                
+            }
+            if (GUI.Button(new Rect(50, 120, 200, 50), "Rename Button"))
+            {
+                if (Selection.activeGameObject != null)
+                {
+                Rename();
+                   
+                }
+                else
+                {
+                    
+                    Debug.Log(("No selection in hierarchy !!!"));
+                    guide = "No selection in hierarchy !!!";
+                }
+                
+            }
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Guide", EditorStyles.boldLabel);
+           
+            EditorGUILayout.LabelField(guide,GUILayout.Height(30));
+        }
+        
+        
+        
+        [MenuItem("Rename/Rename Fast")]
+         static void Rename()
+        {
+            // Keep track of the currently selected GameObject(s)
+            objectArray = Selection.gameObjects;
+            int n = 0;
+            // Loop through every GameObject in the array above
+            foreach (GameObject GameObject in objectArray)
+         {
+            
+
+            GameObject.name = location + n;
+            n++;
+
+
+
+         }
+
+
+        }
+         
+
+      
+     
+    }
