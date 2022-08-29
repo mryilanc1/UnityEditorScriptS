a398173bc8f6e8a9921bdef398fdd4cdcc13416a
Mükremin Remezan YILANCI <41707639+mryilanc1@users.noreply.github.com>
2022-08-29 22:38:40 +0300

    naming was done in the correct order and optimization
    1) "objectArray = Selection.gameObjects " is not  optimization . 
    
    That's why I used that:   "objectArray = Selection.transforms;"  ... it is better .
    
    2) naming was done in the correct order

--- Rename_Editor.cs
+++ Rename_Editor.cs
@@ -1,5 +1,5 @@
 
-
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
@@ -8,89 +8,91 @@ using UnityEngine.UI;
 
 
 public class Rename_Editor : EditorWindow
-    {
+{
 
-        static string location = "";
-        static string localPath;
-        private static string guide = "Select GameObjects";
-        public static GameObject[] objectArray;
+    static string location = "";
+    static string localPath;
+    private static string guide = "Select GameObjects";
+    public  Transform[] objectArray;
     private static Vector2 _windowsMinSize = Vector2.one * 500f;
     private static Rect _helpRect = new Rect(0f, 0f, 400f, 100f);
     private static Rect _listRect = new Rect(Vector2.zero, _windowsMinSize);
 
+
     // Creates a new menu item 'Multiple Prefab/Multiple Prefab Creator  Window' in the main menu and create a shortcut.
     [MenuItem("Rename/Rename_Editor Window")]
-        static void Init()
-        {
+    static void Init()
+    {
 
         // Get existing open window or if none, make a new one:
-        Rename_Editor window = (Rename_Editor) EditorWindow.GetWindow(typeof(Rename_Editor),true, "Rename_Editor");
-          
-            window.Show();
-            
-        }
+        Rename_Editor window = (Rename_Editor)EditorWindow.GetWindow(typeof(Rename_Editor), true, "Rename_Editor");
+    
+        window.Show();
+
+    }
+
+    void OnGUI()
+    {
         
-        void OnGUI()
+     
+        location = EditorGUILayout.TextField("Name:", location);
+        if (Selection.activeGameObject != null)
         {
+            GUI.backgroundColor = Color.green;
 
-      
+        }
+        else
+        {
+            GUI.backgroundColor = Color.red;
 
-            location = EditorGUILayout.TextField("Name:", location);
+        }
+        if (GUI.Button(new Rect(50, 120, 200, 50), "Rename Button"))
+        {
             if (Selection.activeGameObject != null)
             {
-                GUI.backgroundColor= Color.green;
-                
+                Rename();
             }
             else
             {
-                GUI.backgroundColor= Color.red;
-                
+                Debug.Log(("No selection in hierarchy !!!"));
+                guide = "No selection in hierarchy !!!";
             }
-            if (GUI.Button(new Rect(50, 120, 200, 50), "Rename Button"))
-            {
-                if (Selection.activeGameObject != null)
-                {
-                Rename();
-                   
-                }
-                else
-                {
-                    
-                    Debug.Log(("No selection in hierarchy !!!"));
-                    guide = "No selection in hierarchy !!!";
-                }
-                
-            }
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Guide", EditorStyles.boldLabel);
-           
-            EditorGUILayout.LabelField(guide,GUILayout.Height(30));
-        }
-        
-        
-        
-        [MenuItem("Rename/Rename Fast")]
-         static void Rename()
-        {
-            // Keep track of the currently selected GameObject(s)
-            objectArray = Selection.gameObjects;
-            int n = 0;
-            // Loop through every GameObject in the array above
-            foreach (GameObject GameObject in objectArray)
-         {
-            
-
-            GameObject.name = location + n;
-            n++;
-
-
-
-         }
-
 
         }
-         
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Guide", EditorStyles.boldLabel);
 
-      
-     
+        EditorGUILayout.LabelField(guide, GUILayout.Height(30));
     }
+
+
+
+    [MenuItem("Rename/Rename Fast")]
+     void Rename()
+    {
+        // Keep track of the currently selected GameObject(s)
+        objectArray = Selection.transforms;
+        objectArray =  objectArray.OrderBy(go => go.transform.GetSiblingIndex()).ToArray();
+
+        int n = 0;
+        // Loop through every GameObject in the array above
+        foreach (Transform GameObject in objectArray)
+        {
+
+
+            GameObject.transform.name = location + n;
+            n++;
+            
+            Debug.Log(GameObject.name);
+
+
+
+        }
+
+
+    }
+
+
+
+
+}
