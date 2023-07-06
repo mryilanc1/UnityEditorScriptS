baab53364489b839be855eb9f016422cb5b8f1b4
Mükremin Ramazan YILANCI <41707639+mryilanc1@users.noreply.github.com>
2023-07-06 21:01:12 +0300

    pivot and geometric center check

---
+++ MeshPivotChecker.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MeshPivotChecker : EditorWindow
+{
+    public List<GameObject> PivotProblemList = new List<GameObject>();
+    private Vector2 scrollPosition;
+    [MenuItem("Araclar/Mesh Pivot Checker")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MeshPivotChecker));
+    }
+    private float threshold = 8000f;
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Mesh Pivot Checker", EditorStyles.boldLabel);
+        threshold = EditorGUILayout.FloatField("Threshold", threshold);
+        if (GUILayout.Button("Check Pivot for All Objects"))
+        {
+            PivotProblemList.Clear();
+            GameObject[] objects = FindObjectsOfType<GameObject>();
+            foreach (GameObject obj in objects)
+            {
+                MeshFilter meshFilter = obj.GetComponent<MeshFilter>();
+                
+                Bounds bounds = obj.GetComponent<MeshRenderer>().bounds;
+                Vector3 center = bounds.center;
+                Vector3 size = bounds.size;
+                
+                if (meshFilter != null && meshFilter.sharedMesh != null && !meshFilter.transform.name.Contains("JumpPoint") && !meshFilter.transform.name.Contains("NAVMESH")) 
+                {
+                    Vector3 geometricCenter = center ;
+                    Vector3 pivot = obj.transform.position;
+
+                    float distanceX = Mathf.Abs(pivot.x - geometricCenter.x);
+                    float distanceY = Mathf.Abs(pivot.y - geometricCenter.y);
+                    float distanceZ = Mathf.Abs(pivot.z - geometricCenter.z);
+
+                    if (distanceX > threshold || distanceY > threshold || distanceZ > threshold)
+                    { 
+                        Debug.Log(  $"Parent::{obj.transform.parent.parent.name } --- sorunlu:  {obj.name}  Mesh pivot is not at the geometric center.");
+                       PivotProblemList.Add(obj);
+                        
+                    }
+                }
+            }
+        }
+        
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+        ScriptableObject scriptableObj = this;
+        SerializedObject serialObj = new SerializedObject (scriptableObj);
+        SerializedProperty serialProp = serialObj.FindProperty ("PivotProblemList");
+        EditorGUILayout.PropertyField (serialProp, true);
+        serialObj.ApplyModifiedProperties ();
+        
+        EditorGUILayout.EndScrollView();
+        
+    }
+
+    private Vector3 GetGeometricCenter(Mesh mesh)
+    {
+        Vector3[] vertices = mesh.vertices;
+        Vector3 center = Vector3.zero;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            center += vertices[i];
+        }
+
+        center /= vertices.Length;
+
+        return center;
+    }
+}
